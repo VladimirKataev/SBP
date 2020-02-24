b0bdb426cf68ce79d013f6d90b5edb457f2956c6
Vladimir Kataev <vova.kataev@gmail.com>
2020-02-24 07:31:45 -0800

    Debt Calculator stuff

---
+++ lisp/ch1.lisp
@@ -0,0 +1,77 @@
+; "Ansi common lisp, by Paul Graham"
+
+;Ch1
+
+(print (+ (- 5 1) (+ 3 7))) ; returns 4 + 10, or 14
+
+(print (list 1 (+ 2 3))) ; returns (1 5)
+
+(print (if (listp 1)  (+ 1 2) (+ 3 4)))
+				; if '1' is a list, it returns 3. if not, 7
+				; PS, it's not, so 7
+
+(print (list (and (listp 3) t ) (+ 1 2))) ;creates (nil 3)
+
+
+; 3 cons expressions to return (a b c)
+(print (cons 1 (list 2 3)))
+; errrr, wat?
+
+
+
+; funtion to car and cdr to return list[3]
+(defun myFourth (list) 
+	(
+		car (
+			cdr (
+				cdr (
+					cdr list
+				)
+			)
+		)
+	)
+)
+
+
+(print (myFourth (list 1 2 3 4 5)))
+
+
+
+; function that returns the greater of the two args
+(defun greater (a b)
+	(if (> a b) 
+		a 
+		b
+	)
+)
+(print (greater 69 420))
+
+
+; what functions do
+
+(defun enigma (x)
+	(and	(not (null x))		; returns if list isnt empty and
+		(or	(null (car x))	; either first element !nil
+			(enigma (cdr x)); or same is true for later
+		)
+	)
+)
+; so it checks if there's a nil in the list or not
+
+(print (enigma (list 1 'a' nil 1.3)))
+(print (enigma (list 1 'a' 1.3)))
+
+(defun mystery (x y)
+	(if (null y)					;if y is nil, return nil
+		nil
+		(if (eql (car y) x)			;else, if first of y is x
+			0				; return 0
+			(let ((z (mystery x (cdr y))))  ;else z is 
+				(and z (+ z 1)))
+		)
+	)
+)
+; checks for x in y, gives index from 0
+
+(print (mystery 3 (list 1 2 3 4)))
+(print (mystery 3 (list 1 2 4)))
