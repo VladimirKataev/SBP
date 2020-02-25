d6a2faee8b69ce5926f019506c979102b24c2249
Vladimir Kataev <vova.kataev@gmail.com>
2020-02-24 18:59:28 -0800

    Ch1 all but last one

--- lisp/ch1.lisp
+++ lisp/ch1.lisp
@@ -2,19 +2,19 @@
 
 ;Ch1
 
-(print (+ (- 5 1) (+ 3 7))) ; returns 4 + 10, or 14
+;(print (+ (- 5 1) (+ 3 7))) ; returns 4 + 10, or 14
 
-(print (list 1 (+ 2 3))) ; returns (1 5)
+;(print (list 1 (+ 2 3))) ; returns (1 5)
 
-(print (if (listp 1)  (+ 1 2) (+ 3 4)))
+;(print (if (listp 1)  (+ 1 2) (+ 3 4)))
 				; if '1' is a list, it returns 3. if not, 7
 				; PS, it's not, so 7
 
-(print (list (and (listp 3) t ) (+ 1 2))) ;creates (nil 3)
+;(print (list (and (listp 3) t ) (+ 1 2))) ;creates (nil 3)
 
 
 ; 3 cons expressions to return (a b c)
-(print (cons 1 (list 2 3)))
+;(print (cons 1 (list 2 3)))
 ; errrr, wat?
 
 
@@ -32,8 +32,7 @@
 	)
 )
 
-
-(print (myFourth (list 1 2 3 4 5)))
+;(print (myFourth (list 1 2 3 4 5)))
 
 
 
@@ -44,10 +43,9 @@
 		b
 	)
 )
-(print (greater 69 420))
+;(print (greater 69 420))
 
 
-; what functions do
 
 (defun enigma (x)
 	(and	(not (null x))		; returns if list isnt empty and
@@ -58,8 +56,10 @@
 )
 ; so it checks if there's a nil in the list or not
 
-(print (enigma (list 1 'a' nil 1.3)))
-(print (enigma (list 1 'a' 1.3)))
+;(print (enigma (list 1 'a' nil 1.3)))
+;(print (enigma (list 1 'a' 1.3)))
+
+
 
 (defun mystery (x y)
 	(if (null y)					;if y is nil, return nil
@@ -73,5 +73,100 @@
 )
 ; checks for x in y, gives index from 0
 
-(print (mystery 3 (list 1 2 3 4)))
-(print (mystery 3 (list 1 2 4)))
+;(print (mystery 3 (list 1 2 3 4)))
+;(print (mystery 3 (list 1 2 4)))
+
+
+
+
+;(print
+;	(car (car (cdr '(a (b c) d))))
+;)
+;>B
+;x was 'car'
+
+
+
+;(print
+;	(or 13 (/ 1 0))
+;)
+;>13
+;x was 'or'
+
+
+
+;(print
+;	(apply #'list 1 nil)
+;)
+;>(1)
+;x was 'apply'
+
+
+
+;function that returns true if an item within list is a list
+(defun hasNestedList (listIn) ;note, end of list is tested as itself
+	(and
+		listIn
+		(or 
+			(listp (car listIn))
+			(hasNestedList (cdr listIn))
+		)
+	)
+)
+
+;(print (hasNestedList (list 1 2 3 4)))
+;(print (hasNestedList (list 1 (list 2 3) 4)))
+
+
+(defun nDotsIter (n) 
+	(do	(
+		(i n (- i 1))
+		)
+		
+		((< i 0))
+		(print ".")
+	)
+)
+
+
+(defun nDotsRecurs (n)
+	(print ".")
+	(if (> n 0)
+		(nDotsRecurs (- n 1))
+	)
+)
+
+;(nDotsIter 5)
+;(nDotsRecurs 5)
+
+
+(defun elemCountIter (elem inList)
+	(do	(
+			(srch inList (cdr srch))
+			(n 0 (if (= (car srch) elem) (+ n 1) n))
+		)
+		((not srch) n)
+		
+	)
+
+)
+
+(print (elemCountIter 69 (list 69 2 4 69)))
+
+
+(defun elemCounterRecurs (elem inList)
+	(if	inList
+		(if (= elem (car inList))
+			(+ 1 (elemCounterRecurs elem (cdr inList) ))
+			(elemCounterRecurs elem (cdr inList))
+		)
+		0
+	)
+)
+
+(print 
+	(elemCounterRecurs 69 (list 69 2 4 69))
+)
+
+
+
